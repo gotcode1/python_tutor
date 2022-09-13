6dad8c11991c0525404b619f8c595b186e5ff782
gotcode1 <sergiu.tacu@fsg.ulaval.ca>
2022-09-13 14:37:54 -0400

    added checkio home and init files

---
+++ checkio_home.py
@@ -0,0 +1,11 @@
+# Three Words
+"""You are given a string with words and numbers separated by whitespaces (one space). 
+The words contains only letters. You should check if the string contains three words in succession . 
+For example, the string "start 5 one two three 7 end" contains three words in succession.
+
+Input: A string with words.
+Output: The answer as a boolean.
+"""
+
+def checkio_words(words):
+    pass
